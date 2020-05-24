ba46a888a2b7d5da08e360e7661f6a9315225177
rrokkam <rohithrokkam@gmail.com>
2020-05-23 23:18:54 -0700

    Remove local variable score in getScoreForAdvantageGame

--- go/tenniskata/TennisGame1.go
+++ go/tenniskata/TennisGame1.go
@@ -39,18 +39,16 @@ func getScoreForEvenGame(score1 int) string {
 }
 
 func getScoreForAdvantageGame(score1, score2 int) string {
-	score := ""
 	minusResult := score1 - score2
 	if minusResult == 1 {
-		score = "Advantage player1"
+		return "Advantage player1"
 	} else if minusResult == -1 {
-		score = "Advantage player2"
+		return "Advantage player2"
 	} else if minusResult >= 2 {
-		score = "Win for player1"
+		return "Win for player1"
 	} else {
-		score = "Win for player2"
+		return "Win for player2"
 	}
-	return score
 }
 
 func getScoreForRegularGame(score1, score2 int) string {
