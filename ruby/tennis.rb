2e3473f596e4ad8fcc70daf4cfcd2e13a16c6064
rrokkam <rohithrokkam@gmail.com>
2020-05-25 14:34:55 -0700

    Casing, commas, other twiddles to satisfy rubocop

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -19,7 +19,7 @@ module Tennis
       return tied_score if tied?
       return advantage_or_won_score if advantage_or_won?
 
-      return regular_score
+      regular_score
     end
 
     def tied?
@@ -28,10 +28,10 @@ module Tennis
 
     def tied_score
       {
-        0 => "Love-All",
-        1 => "Fifteen-All",
-        2 => "Thirty-All",
-      }.fetch(@server_points, "Deuce")
+        0 => 'Love-All',
+        1 => 'Fifteen-All',
+        2 => 'Thirty-All'
+      }.fetch(@server_points, 'Deuce')
     end
 
     def advantage_or_won?
@@ -39,15 +39,15 @@ module Tennis
     end
 
     def advantage_or_won_score
-      minusResult = @server_points - @receiver_points
-      if (minusResult == 1)
-        "Advantage player1"
-      elsif (minusResult == -1)
-        "Advantage player2"
-      elsif (minusResult >= 2)
-        "Win for player1"
+      minus_result = @server_points - @receiver_points
+      if minus_result == 1
+        'Advantage player1'
+      elsif minus_result == -1
+        'Advantage player2'
+      elsif minus_result >= 2
+        'Win for player1'
       else
-        "Win for player2"
+        'Win for player2'
       end
     end
 
@@ -60,25 +60,22 @@ module Tennis
         0 => 'Love',
         1 => 'Fifteen',
         2 => 'Thirty',
-        3 => 'Forty',
+        3 => 'Forty'
       }
     end
   end
 end
 
 class TennisGame1
-  def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
+  def initialize(server_name, receiver_name)
+    @server_name = server_name
+    @receiver_name = receiver_name
     @score = Tennis::GameScore.new
   end
 
-  def won_point(playerName)
-    if playerName == @player1Name
-      @score = @score.server_scored
-    else
-      @score = @score.receiver_scored
-    end
+  def won_point(name)
+    @score = @score.server_scored if name == @server_name
+    @score = @score.receiver_scored if name == @receiver_name
   end
 
   def score
