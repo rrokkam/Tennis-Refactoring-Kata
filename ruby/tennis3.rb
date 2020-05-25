7c5f12d9626a969ccae081694512ae220a39ed86
rrokkam <rohithrokkam@gmail.com>
2020-05-25 15:28:46 -0700

    Rubocop -a on tennis3

--- ruby/tennis3.rb
+++ ruby/tennis3.rb
@@ -1,31 +1,31 @@
+# frozen_string_literal: true
+
 class TennisGame3
-  def initialize(player1Name, player2Name)
-    @p1N = player1Name
-    @p2N = player2Name
-    @p1 = 0
-    @p2 = 0
+  def initialize(player1_name, player2_name)
+    @p1_name = player1_name
+    @p2_name = player2_name
+    @p1_score = 0
+    @p2_score = 0
   end
 
-  def won_point(n)
-    if n == @p1N
-      @p1 += 1
+  def won_point(name)
+    if name == @p1_name
+      @p1_score += 1
     else
-      @p2 += 1
+      @p2_score += 1
     end
   end
 
   def score
-    if (@p1 < 4 and @p2 < 4) and (@p1 + @p2 < 6)
-      p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@p1]
-      @p1 == @p2 ? s + "-All" : s + "-" + p[@p2]
+    if ((@p1_score < 4) && (@p2_score < 4)) && (@p1_score + @p2_score < 6)
+      p = %w[Love Fifteen Thirty Forty]
+      s = p[@p1_score]
+      @p1_score == @p2_score ? s + '-All' : s + '-' + p[@p2_score]
+    elsif @p1_score == @p2_score
+      'Deuce'
     else
-      if (@p1 == @p2)
-        "Deuce"
-      else
-        s = @p1 > @p2 ? @p1N : @p2N
-        (@p1 - @p2) * (@p1 - @p2) == 1 ? "Advantage " + s : "Win for " + s
-      end
+      s = @p1_score > @p2_score ? @p1_name : @p2_name
+      (@p1_score - @p2_score) * (@p1_score - @p2_score) == 1 ? 'Advantage ' + s : 'Win for ' + s
     end
   end
 end
