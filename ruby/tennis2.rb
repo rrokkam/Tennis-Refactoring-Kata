7f6ca3f572f97891e80666d58351cab46adff087
rrokkam <rohithrokkam@gmail.com>
2020-05-26 08:18:28 -0700

    Remove useless condition in if statement

--- ruby/tennis2.rb
+++ ruby/tennis2.rb
@@ -18,15 +18,15 @@ class TennisGame2
   end
 
   def score
-    result = ''
     return label[@p1points] + '-All' if (@p1points == @p2points) && (@p1points < 3)
     return 'Deuce' if (@p1points == @p2points) && (@p1points >= 3)
     return label[@p1points] + '-' + label[@p2points] if [@p1points, @p2points].max < 4
 
+    result = ''
     result = 'Advantage ' + @player1_name if (@p1points > @p2points) && (@p2points >= 3)
     result = 'Advantage ' + @player2_name if (@p2points > @p1points) && (@p1points >= 3)
-    result = 'Win for ' + @player1_name if (@p1points >= 4) && (@p2points >= 0) && ((@p1points - @p2points) >= 2)
-    result = 'Win for ' + @player2_name if (@p2points >= 4) && (@p1points >= 0) && ((@p2points - @p1points) >= 2)
+    result = 'Win for ' + @player1_name if (@p1points >= 4) && ((@p1points - @p2points) >= 2)
+    result = 'Win for ' + @player2_name if (@p2points >= 4) && ((@p2points - @p1points) >= 2)
     result
   end
 end
