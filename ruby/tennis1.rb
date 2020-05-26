02cc09d0f8293952c1a64b78947ae6015ca67f81
rrokkam <rohithrokkam@gmail.com>
2020-05-25 18:10:34 -0700

    Remove GameScore class

--- ruby/tennis1.rb
+++ ruby/tennis1.rb
@@ -56,57 +56,31 @@ module Score
   end
 end
 
-module Tennis
-  class GameScore
-    def initialize
-      @server_points = 0
-      @receiver_points = 0
-    end
-
-    def server_scored
-      @server_points += 1
-      self
-    end
-
-    def receiver_scored
-      @receiver_points += 1
-      self
-    end
-
-    def score
-      return Score::Tied.new(@server_points) if tied?
-      return Score::AdvantageOrWon.new(@server_points, @receiver_points) if advantage_or_won?
-
-      Score::Regular.new(@server_points, @receiver_points)
-    end
-
-    def to_str
-      score.to_str
-    end
-
-    def tied?
-      @server_points == @receiver_points
-    end
-
-    def advantage_or_won?
-      @server_points >= 4 or @receiver_points >= 4
-    end
-  end
-end
-
 class TennisGame1
   def initialize(server_name, receiver_name)
     @server_name = server_name
     @receiver_name = receiver_name
-    @score = Tennis::GameScore.new
+    @server_points = 0
+    @receiver_points = 0
   end
 
   def won_point(name)
-    @score = @score.server_scored if name == @server_name
-    @score = @score.receiver_scored if name == @receiver_name
+    @server_points += 1 if name == @server_name
+    @receiver_points += 1 if name == @receiver_name
   end
 
   def score
-    @score.to_str
+    return Score::Tied.new(@server_points).to_str if tied?
+    return Score::AdvantageOrWon.new(@server_points, @receiver_points).to_str if advantage_or_won?
+
+    Score::Regular.new(@server_points, @receiver_points).to_str
+  end
+
+  def tied?
+    @server_points == @receiver_points
+  end
+
+  def advantage_or_won?
+    @server_points >= 4 or @receiver_points >= 4
   end
 end
